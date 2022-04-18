6d9e730d9dbaa9f7fee23fb8a572d08fa060397a
Utkarsh Sharma <rickysharma1111@gmail.com>
2022-04-18 11:34:16 +0530

    Some minor cchanges

--- lib/data/repositories/auth_repo_impl.dart
+++ lib/data/repositories/auth_repo_impl.dart
@@ -1,12 +1,10 @@
 import 'package:basic_structure/data/models/response/login_response/login_response.dart';
 import 'package:basic_structure/data/models/request/login_request/login_request.dart';
-import 'package:basic_structure/domain/repositories/auth_repo.dart';
 
 import '../../core/ utils/network/api_result/api_result.dart';
 import '../../core/ utils/network/network_util/network_util.dart';
 
-class AuthImpl implements AuthRepo {
-  @override
+class AuthRepo {
   Future<ApiResult<LoginResponse>> signIn(
       {required LoginRequest loginRequest}) async {
     try {
--- lib/domain/repositories/auth_repo.dart
+++
@@ -1,7 +0,0 @@
-import '../../core/ utils/network/api_result/api_result.dart';
-import '../../data/models/request/login_request/login_request.dart';
-import '../../data/models/response/login_response/login_response.dart';
-
-abstract class AuthRepo {
-  Future<ApiResult<LoginResponse>> signIn({required LoginRequest loginRequest});
-}
